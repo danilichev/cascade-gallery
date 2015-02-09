02c9413540ccafe7e10720e648bb1e5073569436
Valentyn <v.danilichev@gmail.com>
2015-02-09 10:34:46 +0200

    added changes in script structure

--- src/cascade-gallery.js
+++ src/cascade-gallery.js
@@ -1,90 +1,94 @@
-var Gallery = {};
+var gallery = (function() {
 
-Gallery.init = function() {
-
-  var galleryWrapperClassName = "gallery-wrapper",
+  var galleryWrapperId = "gallery-wrapper",
+      thumbnailsWrapperId = "thumbnails-wrapper",
       imagesWrapperId = "images-wrapper",
-      descriptionWrapperClassName = "description-wrapper",
-      nameOfImagesId = "name",
+      descriptionWrapperId = "description-wrapper",
+      titleId = "title",
       closeButtonId = "close-button",
-      descriptionOfImagesId = "description",
-      thumbnailsWrapperClassName = "thumbnails-wrapper";
+      descriptionId = "description";
 
+  var galleryWrapper = document.getElementById(galleryWrapperId);
 
-  var showImages = function(settings) {
-
-    var thumbnailId = settings.thumbnailId,
-        name = settings.name,
-        decription = settings.description,
-        images = settings.images;
-
-    var parent =
-        document.getElementsByClassName(galleryWrapperClassName)[0];
-
-    var removeCurrentImagesWrapper = function() {
-      var currentImagesWrapper = document.getElementById(imagesWrapperId);
-      if (currentImagesWrapper != null) {
-        parent.removeChild(document.getElementById(imagesWrapperId));
-      }
-    }
-
-    removeCurrentImagesWrapper();
-
-    var imagesWrapper = document.createElement("div");
-    imagesWrapper.id = imagesWrapperId;
-
-    var decriptionWrapper = document.createElement("div");
-    decriptionWrapper.className = descriptionWrapperClassName;
-
-    var closeButton = document.createElement("div");
-    closeButton.id = closeButtonId;
-    closeButton.onclick = function() { removeCurrentImagesWrapper(); }
-    decriptionWrapper.appendChild(closeButton);
-
-    var nameOfImages = document.createElement("div");
-    nameOfImages.id = nameOfImagesId;
-    nameOfImages.innerHTML = name;
-    decriptionWrapper.appendChild(nameOfImages);
-
-    if (decription != null) {
-      var descriptionOfImages = document.createElement("div");
-      descriptionOfImages.id = descriptionOfImagesId;
-      descriptionOfImages.innerHTML = decription;
-
-      decriptionWrapper.appendChild(descriptionOfImages);
-    }
-
-    imagesWrapper.appendChild(decriptionWrapper);
-
-    var addImagesToImagesWrapper = function() {
-      var i, image, lineBreak;
-      for (i = 0; i < images.length; i++) {
-        image = document.createElement("img");
-        image.src = images[i];
-        imagesWrapper.appendChild(image);
-
-        lineBreak = document.createElement("br");
-        imagesWrapper.appendChild(lineBreak);
-      }
-    }();
-
-    var thumbnailsWrapper =
-        document.getElementsByClassName(thumbnailsWrapperClassName)[0];
-    parent.insertBefore(imagesWrapper, thumbnailsWrapper);
+  var removeImagesWrapperIfExist = function() {
+    var imagesWrapper = document.getElementById(imagesWrapperId);
+    if (imagesWrapper != null) galleryWrapper.removeChild(imagesWrapper);
   }
 
-  var helper = function(settings) {
+  var addImagesToWrapper = function(wrapper, imagesList) {
+    var i, image, lineBreak;
+    for (i = 0; i < imagesList.length; i++) {
+      image = document.createElement("img");
+      image.src = imagesList[i];
+      wrapper.appendChild(image);
+
+      lineBreak = document.createElement("br");
+      wrapper.appendChild(lineBreak);
+    }
+  }
+
+  var createDivWithId = function(id) {
+    var div = document.createElement("div");
+    div.id = id;
+    return div;
+  }
+
+  var createDescriptionWrapper = function(title, description) {
+    var wrapper = createDivWithId(descriptionWrapperId);
+
+    var closeButton = createDivWithId(closeButtonId);
+    closeButton.onclick = function() { removeImagesWrapperIfExist(); };
+    wrapper.appendChild(closeButton);
+
+    var titleBlock = createDivWithId(titleId);
+    titleBlock.innerHTML = title;
+    wrapper.appendChild(titleBlock);
+
+    if (description != null) {
+      var descriptionBlock = createDivWithId(descriptionId);
+      descriptionBlock.innerHTML = description;
+      wrapper.appendChild(descriptionBlock);
+    }
+
+    return wrapper;
+  }
+
+  var showImages = function(settings) {
+    var name = settings.name,
+        description = settings.description,
+        images = settings.images;
+
+    removeImagesWrapperIfExist();
+
+    var imagesWrapper = createDivWithId(imagesWrapperId);
+
+    var descriptionWrapper = createDescriptionWrapper(name, description);
+    imagesWrapper.appendChild(descriptionWrapper);
+
+    addImagesToWrapper(imagesWrapper, images);
+
+    var thumbnailsWrapper = document.getElementById(thumbnailsWrapperId);
+    galleryWrapper.insertBefore(imagesWrapper, thumbnailsWrapper);
+  }
+
+  var showImagesHelper = function(settings) {
     return function() {
       showImages(settings);
     }
   }
 
-  var thumbnails = [], i;
-  for (i = 0; i < arguments.length; i++) {
-    var argument = arguments[i];
+  var init = function() {
+    var thumbnails = [], i;
+    for (i = 0; i < arguments.length; i++) {
+      var argument = arguments[i];
 
-    thumbnails[i] = document.getElementById(argument.thumbnailId);
-    thumbnails[i].onclick = helper(argument);
-    thumbnails[i].href = "#";
+      thumbnails[i] = document.getElementById(argument.thumbnailId);
+      thumbnails[i].onclick = showImagesHelper(argument);
+      thumbnails[i].href = "#";
+    }
   }
-}
+
+  return {
+    init: init
+  }
+})();
